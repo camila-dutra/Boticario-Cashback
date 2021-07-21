038156cfca6ce2b27d1fd6ded4ba90cd2e85a19b
Camila <cmartinsdutra@gmail.com>
2021-07-21 13:30:31 -0300

    project structure

---
+++ Cashback.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cashback.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Cashback.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cashback.Service
+{
+    public class Class1
+    {
+    }
+}
